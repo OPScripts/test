6ec9aa8a610a95709db952311a7c0f1550d7cabe
OPScripts <128308014+OPScripts@users.noreply.github.com>
2025-10-07 03:08:05 +0800

    Add files via upload

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,39 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const ticketSchema = new mongoose.Schema({
     ticketNumber: {
         type: Number,
-        required: true,
+        required: true
     },
     channelId: {
         type: String,
         required: true,
-        unique: true,
+        unique: true
     },
     userId: {
         type: String,
-        required: true,
+        required: true
     },
     category: {
         type: String,
-        required: true,
+        required: true
     },
     fields: {
         type: Array,
-        default: [],
+        default: []
     },
     selectedHelpers: {
         type: Array,
-        default: [],
+        default: []
     },
     completedBy: {
         type: String,
-        default: null,
+        default: null
     },
     status: {
         type: String,
-        default: "open",
-    },
-});
+        default: 'active'
+    }
+}, { timestamps: true });
 
-module.exports = mongoose.model("Ticket", ticketSchema);
+module.exports = mongoose.model('Ticket', ticketSchema);
